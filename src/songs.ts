e6081de7f7aef1b015a46abf3524241c2c8e73e9
Alex Uribe <repzyyog@gmail.com>
2025-02-11 12:31:10 +0100

    Show number of songs in playlist

--- src/songs.ts
+++ src/songs.ts
@@ -39,9 +39,8 @@ export const getErrorMessage = (errorCode: string): string => {
 
 export const getSongsCount = (songs: Song[]): number => {
   let songsCount: number;
-  // Asígnale a la variable songsCount el número de canciones recibidas
 
-  songsCount = 0; // Cambia esta línea por tu código
+  songsCount = songs.length;
 
   return songsCount;
 };
